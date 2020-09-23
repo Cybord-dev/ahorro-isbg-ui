bbf4d00cf0170ce403fc514210b4a3f7eaf46226
Eddieson <editiend@gmail.com>
2020-09-23 11:32:30 -0500

    servicios y modelos
    catalogos, saldoahorro, saldoprestamo y datosusuario

--- src/app/services/ahorro.service.ts
+++ src/app/services/ahorro.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { SaldoAhorro } from '../models/saldoahorro';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AhorroService {
+export class AhorroServicio {
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  public getSaldoByUsuario (idUsuario: number): Observable<SaldoAhorro[]>{
+    return this.http.get<SaldoAhorro[]>('../usuarios/${idUsuario}/ahorros');
+  }
+
+  public postSaldo (idUsuario: number, saldo: SaldoAhorro): Observable<SaldoAhorro[]>{
+    return this.http.post<SaldoAhorro[]>('../usuarios/${idUsuario}/ahorro', saldo);
+  }
+
+  public getSaldoByUsuarioAndId (idUsuario: number, idSaldo: number): Observable<SaldoAhorro[]>{
+    return this.http.get<SaldoAhorro[]>('../usuarios/{idUsuario}/ahorros/{idAhorro}');
+  }
+
 }
+
