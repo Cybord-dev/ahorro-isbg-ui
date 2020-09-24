a2cac1a1006f1529be0fcd80c9c6b098d7643a7f
ralfdemoledor <rubens.mncrft@gmail.com>
2020-09-23 22:28:14 -0500

    Adding routes and building menu

--- src/app/pages/administracion/administracion-routing.module.ts
+++ src/app/pages/administracion/administracion-routing.module.ts
@@ -2,8 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReporteSolicitudesComponent } from '../commons/reporte-solicitudes/reporte-solicitudes.component';
 import { HistoricoSolicitudesComponent } from '../commons/historico-solicitudes/historico-solicitudes.component';
+import { UsuariosComponent } from '../commons/usuarios/usuarios.component';
+import { AltaRolesComponent } from './alta-roles/alta-roles.component';
 
 const routes: Routes = [
+  {
+    path: 'usuarios',
+    component: UsuariosComponent,
+    data: {
+      title: 'Usuarios'
+    }
+  },
+  {
+    path: 'usuarios/:idUsuario',
+    component: AltaRolesComponent,
+    data: {
+      title: 'Usario ISBG'
+    }
+  },
   {
     path: 'validaciones',
     component: ReporteSolicitudesComponent,
--- src/app/pages/contabilidad/contabilidad-routing.module.ts
+++ src/app/pages/contabilidad/contabilidad-routing.module.ts
@@ -4,8 +4,24 @@ import { ReporteSolicitudesComponent } from '../commons/reporte-solicitudes/repo
 import { HistoricoSolicitudesComponent } from '../commons/historico-solicitudes/historico-solicitudes.component';
 import { ConciliacionContaComponent } from './conciliacion-conta/conciliacion-conta.component';
 import { AhorroExternosComponent } from './ahorro-externos/ahorro-externos.component';
+import { UsuariosComponent } from '../commons/usuarios/usuarios.component';
+import { UsuarioComponent } from '../commons/usuario/usuario.component';
 
 const routes: Routes = [
+  {
+    path: 'usuarios',
+    component: UsuariosComponent,
+    data: {
+      title: 'Usuarios'
+    }
+  },
+  {
+    path: 'usuarios/:idUsuario',
+    component: UsuarioComponent,
+    data: {
+      title: 'Usario Externo'
+    }
+  },
   {
     path: 'validaciones',
     component: ReporteSolicitudesComponent,
