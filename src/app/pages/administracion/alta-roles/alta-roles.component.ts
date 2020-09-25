290b04a897638d28d26bffc4618ebee8d31d60a2
ralfdemoledor <rubens.mncrft@gmail.com>
2020-09-24 23:48:53 -0500

    Adding roles view

--- src/app/pages/administracion/alta-roles/alta-roles.component.ts
+++ src/app/pages/administracion/alta-roles/alta-roles.component.ts
@@ -2,11 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../../../models/usuario';
 import { UsuariosService } from '../../../services/usuarios.service';
 import { ActivatedRoute } from '@angular/router';
-import { from } from 'rxjs';
-
-interface IHash {
-  [key: string]: boolean;
-}
 
 @Component({
   selector: 'cybord-alta-roles',
@@ -18,8 +13,9 @@ export class AltaRolesComponent implements OnInit {
   public loading = true;
   public usuario: Usuario = new Usuario();
   public errorMessages: string[] = [];
+  public success = '';
 
-  public roles: IHash = { USUARIO: false, RECURSOS_HUMANOS: false, TESORERIA: false,CONTABILIDAD: false, GERENCIA: false, ADMINISTRACION: false};
+  public roles = { USUARIO: false, RECURSOS_HUMANOS: false, TESORERIA: false, CONTABILIDAD: false, GERENCIA: false, ADMINISTRACION: false};
 
   constructor(
     private route: ActivatedRoute,
@@ -31,14 +27,22 @@ export class AltaRolesComponent implements OnInit {
     this.route.paramMap.subscribe(route => {
       const id = +route.get('idUsuario');
       this.userService.getUsuario(id).subscribe((user: Usuario) => {
+        this.usuario = user;
         for (const role of user.roles) {
           this.roles[role.rolname.nombre] = true;
         }
-        console.log(this.roles);
       });
-
     });
   }
+
+
+  public  updateRoles() {
+    console.log('Updating user roles');
+    this.success = "La informacion de usuario ha sido actualizada";
+  }
+
+
+
 
 
 }
