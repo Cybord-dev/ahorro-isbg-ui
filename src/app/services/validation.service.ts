152476c82e72f0f87b381b2ac186f6357cd0e831
ralfdemoledor <rubens.mncrft@gmail.com>
2020-11-15 14:05:55 -0600

    Validation example

--- src/app/services/validation.service.ts
+++ src/app/services/validation.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Solicitud } from '../models/solicitud';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,20 @@ import { Injectable } from '@angular/core';
 export class ValidationService {
 
   constructor() { }
+
+
+
+  public validateSolicitud(solicitud: Solicitud, totalAhorro: number): string[]{
+    const alerts: string[] = [];
+    if ('RetiroParcialAhorro' === solicitud.tipo && +solicitud.atributos.MONTO > totalAhorro){
+      alerts.push(`No es posible solicitar un monto superior al total de su ahorro de $${totalAhorro}`);
+    }
+    if (solicitud.atributos.MONTO === undefined){
+      alerts.push('El monto de la solicitud no ha sido asignado');
+    }
+    if ('CancelacionAhorro' === solicitud.tipo && +solicitud.atributos.RAZON_CANCELACION.length < 10){
+      alerts.push(`Razon o motivos de cancelacion de ahorros insuficientes`);
+    }
+    return alerts;
+  }
 }
