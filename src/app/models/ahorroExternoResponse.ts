e92305d313d4df53ac88f3cc52969fceeccc9656
Cristian Escamilla - c0e027c <escamilla.cristian@gmail.com>
2020-11-15 22:35:17 -0600

    Corrigendo cargaAhorroExternos

---
+++ src/app/models/ahorroExternoResponse.ts
@@ -0,0 +1,9 @@
+export class AhorroExternoResponse {
+
+    public idUsuario: number;
+    public noEmpleado: string;
+    public nombre: string;
+    public observaciones: string;
+    public saldo: number;
+
+}
