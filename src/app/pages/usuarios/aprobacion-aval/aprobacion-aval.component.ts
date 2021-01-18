a304f057a240a344aa5ce79b6ed8ea14e35842c3
Eddieson <editiend@gmail.com>
2021-01-17 18:07:43 -0600

    avales version inicial

--- src/app/pages/usuarios/aprobacion-aval/aprobacion-aval.component.ts
+++ src/app/pages/usuarios/aprobacion-aval/aprobacion-aval.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModalDirective } from 'ngx-bootstrap/modal/public_api';
+import { GenericPage } from '../../../models/generic-page';
+import { AvalService } from '../../../services/aval.service';
+import { SolicitudesService } from '../../../services/solicitudes.service';
+import { Aval } from '../../../models/aval';
+import { Atributos } from '../../../models/atributos';
 
 @Component({
   selector: 'cybord-aprobacion-aval',
@@ -7,9 +13,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AprobacionAvalComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('modalConfirmacion') public modalConfirmacion: ModalDirective;
+
+  public errorMessages: string[] = [];
+  public mensajeModal = '';
+  public params: any = { success: '', message: '', id: '*', module: 'usuarios' };
+  public page: GenericPage<any> = new GenericPage();
+  public loading = false;
+  public mostrando = false;
+  public informacion: any = {
+    noEmpleado: "", atrib: Atributos, monto: "", nombreDeudor: "", status: ""
+  }
+  
+  constructor(
+    private avalService: AvalService,
+    private solicitudService: SolicitudesService
+    ) { }
 
   ngOnInit(): void {
+  }
+
+  public mostrarInformacion(fila: any): void {
+    
+  }
+
+  public onChangePageSize(pageSize: number): void {
+    
+  }
+
+  public openModal(status:Boolean): void {
+    
+    this.modalConfirmacion.show();
+  }
+
+  public updateDataTable(currentPage?: number, pageSize?: number, filterParams?: any): void{
+    this.loading = true;
+    
   }
 
 }
