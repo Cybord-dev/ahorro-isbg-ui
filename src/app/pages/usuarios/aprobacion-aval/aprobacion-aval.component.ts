7872fc0745c972f2f7472d002ecb16c2293e47b3
Eddieson <editiend@gmail.com>
2021-01-18 18:51:28 -0600

    avales

--- src/app/pages/usuarios/aprobacion-aval/aprobacion-aval.component.ts
+++ src/app/pages/usuarios/aprobacion-aval/aprobacion-aval.component.ts
@@ -3,6 +3,7 @@ import { ModalDirective } from 'ngx-bootstrap/modal/public_api';
 import { GenericPage } from '../../../models/generic-page';
 import { AvalService } from '../../../services/aval.service';
 import { SolicitudesService } from '../../../services/solicitudes.service';
+import { UsuariosService } from '../../../services/usuarios.service';
 import { AceptacionAval } from '../../../models/aceptacion-aval';
 import { Atributos } from '../../../models/atributos';
 import { Usuario } from 'src/app/models/usuario';
@@ -25,15 +26,25 @@ export class AprobacionAvalComponent implements OnInit {
   public informacion: any = {
     noEmpleado: "", atrib: Atributos, monto: "", nombreDeudor: "", status: ""
   }
+  private idusuario: number;
+  private noEmpleado: string;
 
   public usuario : Usuario = new Usuario();
   
   constructor(
     private avalService: AvalService,
-    private solicitudService: SolicitudesService
+    private solicitudService: SolicitudesService,
+    private userService: UsuariosService
     ) { }
 
   ngOnInit(): void {
+    this.userService.myInfo()
+      .then(user => {
+        this.idusuario = user.id
+        this.noEmpleado = user.noEmpleado
+      })
+      .catch(error => this.errorMessages.push(error));
+    
   }
 
   public mostrarInformacion(fila: any): void {
@@ -45,7 +56,6 @@ export class AprobacionAvalComponent implements OnInit {
   }
 
   public openModal(status:Boolean): void {
-    
     this.modalConfirmacion.show();
   }
 
