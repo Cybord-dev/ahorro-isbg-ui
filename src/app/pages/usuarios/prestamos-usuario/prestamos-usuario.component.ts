652b15c40b793ecba2df58ac88bb7249b19aa9d0
Eddieson <editiend@gmail.com>
2021-02-02 20:34:56 -0600

    no te mueras
    porfa

--- src/app/pages/usuarios/prestamos-usuario/prestamos-usuario.component.ts
+++ src/app/pages/usuarios/prestamos-usuario/prestamos-usuario.component.ts
@@ -5,6 +5,7 @@ import { SaldoPrestamo } from 'src/app/models/saldoprestamo';
 import { UsuariosService } from '../../../services/usuarios.service';
 import { ModalDirective } from 'ngx-bootstrap/modal/public_api';
 import { Usuario } from 'src/app/models/usuario';
+import { getMonth } from 'ngx-bootstrap/chronos';
 
 @Component({
   selector: 'cybord-prestamos-usuario',
@@ -20,6 +21,13 @@ export class PrestamosUsuarioComponent implements OnInit {
     private userService: UsuariosService
   ) { }
 
+  public barChartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels: string[] = [];
+  public barChartData: any[] = [{ data: [], label: 'Deuda' }];
+
   public errorMessages: string[] = [];  
   public loading = false;
   public prestamos: Prestamo[] = [];
@@ -29,6 +37,9 @@ export class PrestamosUsuarioComponent implements OnInit {
   private usuario: Usuario;
   public infoTotal: number = 0.0;
   public infoPendiente: number = 0.0;
+  public x0: Date;
+  public x1: Date;
+  public montos: any[] = [];
 
   ngOnInit(): void {
     this.loading = true;
@@ -37,13 +48,23 @@ export class PrestamosUsuarioComponent implements OnInit {
 
   public async loadInfo(){
     try{
+      let fechas: Date[] = [];
       this.usuario = await this.userService.myInfo();
       this.prestamos = await this.prestamoService.getPrestamosPendientesByUsuario(this.usuario.id).toPromise();
       this.prestamos.forEach(e => {
         this.total += e.monto;
         this.totalPendiente += e.saldoPendiente;
+        fechas.push(e.fechaCreacion);
+        e.saldosPrestamo.forEach(b => 
+          fechas.push(b.fechaCreacion)
+        );
+        
       });
-      console.log(this.prestamos);
+      var fechasMap = fechas.map(function(x) { return new Date(x); })
+      this.x1 = new Date(Math.max.apply(null,fechasMap));
+      this.x0 = new Date(Math.min.apply(null,fechasMap));
+      this.createLabels();
+      this.setCharData();
       this.loading = false;
     }catch(error){
       this.errorMessages.push(error);
@@ -71,4 +92,28 @@ export class PrestamosUsuarioComponent implements OnInit {
     this.modalConfirmacion.show();
   }
 
+  private getYearAndMonth(fecha:Date):string{
+    return fecha.getFullYear()+"-"+(fecha.getMonth()+1);
+  }
+
+  private setToZero(fecha:Date):Date{
+    let a = new Date(fecha);
+    a.setHours(0);
+    a.setMinutes(0);
+    a.setMilliseconds(0);
+    a.setDate(1);
+    return a;
+  }
+
+  private createLabels(){
+    this.x0 = this.setToZero(this.x0);
+    this.x1 = this.setToZero(this.x1);
+    while( !(this.x0.getTime()===this.x1.getTime()) ){
+      this.barChartLabels.push(this.getYearAndMonth(this.x0));
+      this.x0.setMonth(this.x0.getMonth()+1);
+    }
+  }
+  private setCharData(){
+    
+  }
 }
